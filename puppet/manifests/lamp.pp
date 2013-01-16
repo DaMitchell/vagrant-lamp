169e5ca84f75849817044794f055631b42adcf04
Daniel Mitchell <danmitchell@ntlworld.com>
2013-01-16 17:07:22 +0000

    new changes include a vhost pointing to ./public

--- puppet/manifests/lamp.pp
+++ puppet/manifests/lamp.pp
@@ -1,6 +1,33 @@
 
+Exec 
+{
+	path => [
+		"/usr/bin", 
+		"/bin", 
+		"/usr/sbin", 
+		"/sbin", 
+		"/usr/local/bin", 
+		"/usr/local/sbin"
+	]
+}
+
+exec {"apt-get update":
+	command => "/usr/bin/apt-get update"
+}
+
 #Install apache
-class {'apache': }
+class {"apache": }
 
 #Install the php apache mod
-class {'apache::mod::php': }
+class {"apache::mod::php": }
+
+file{"/etc/apache2/sites-enabled/default":
+	ensure  => present,
+    source  => "/vagrant/puppet/templates/vhost",
+    require => Package["apache2"],
+	notify  => Service["apache2"]
+}
+
+#Include php
+include php
+
