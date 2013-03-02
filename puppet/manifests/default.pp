fd924c45eaff4b2eeb7f0bc77c0a1863dd13dcf3
Dan Mitchell <danmitchell@ntlworld.com>
2013-03-02 17:09:04 +0000

    update to puppet setup to allow remote connections to mongodb admin ui

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -86,8 +86,13 @@ class setup-php
 class setup-mongodb
 {
 	class { 'mongodb': }
+	
+	exec { 'allow remote mongo connections':
+		command => "/usr/bin/sudo sed -i 's/bind_ip = 127.0.0.1/bind_ip = 0.0.0.0/g' /etc/mongodb.conf",
+		notify  => Service['mongodb'],
+		onlyif  => '/bin/grep -qx  "bind_ip = 127.0.0.1" /etc/mongodb.conf',
+	}	
 }
-
 
 class development 
 {
